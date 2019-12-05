0b70d809d8030ed531ab29842e3f2f13d43a50e5
Gerard Wolfaardt <gerard@thedatacollective.co>
2019-12-06 09:46:29 +1300

    Added scripts

---
+++ Data Lake Architecture using Delta Lake, Databricks and ADLS Gen2 Part 4/SQL Scripts/01_Create_TestDeltaLake_Table.sql
@@ -0,0 +1,16 @@
+-- -----------------------------------------------------
+-- This script creates all the test cases for Delta Lake
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS [dbo].[TestDeltaLake]
+GO
+
+CREATE TABLE [dbo].[TestDeltaLake](
+	[SomeId] [int]  NOT NULL,
+	[SomeNvarchar] [nvarchar](20) NOT NULL,
+	[SomeDateTimeOffset] [datetimeoffset] NULL,
+	[SomeInt] [int] NOT NULL,
+	[SomeDecimal] [decimal] (18,7) NOT NULL,
+	[SomeGUID] [uniqueidentifier] NOT NULL,
+	[SomeDateTime] [datetime] NULL,
+	CONSTRAINT [PK_DeltaLake] PRIMARY KEY CLUSTERED ([SomeId] ASC),
+)
